43ebc0b8d94716134db0ebfb2911bd42e16b9bef
gauri <gaurigera@gmail.com>
2024-06-29 01:48:57 +0530

    Resolve error of deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -6,11 +5,11 @@ import { NextUIProvider } from "@nextui-org/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
         <App />
       </NextThemesProvider>
     </NextUIProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
