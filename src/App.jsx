8ed2be08d5e3ce9e4efd90e13179a12b2646e8c6
gauri <gaurigera@gmail.com>
2024-07-27 23:09:30 +0530

    Added SEO optimizations

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import { Helmet } from "react-helmet";
 
 export default function App() {
   return (
-    <main className="font-mono w-full h-full flex justify-center">
-      <Router>
-        <Routes>
-          <Route path="/portfolio/" element={<Layout />} />
-        </Routes>
-      </Router>
-    </main>
+    <>
+      <Helmet>
+        <title>Gauri Gera - Portfolio</title>
+        <meta
+          name="description"
+          content="Learn about Gauri Gera, an aspiring software developer with keen interest in low-level systems, fullstack development and DevOps."
+        />
+      </Helmet>
+      <main className="font-mono w-full h-full flex justify-center">
+        <Router>
+          <Routes>
+            <Route path="/portfolio/" element={<Layout />} />
+          </Routes>
+        </Router>
+      </main>
+    </>
   );
 }
