ec7ead8db8b40b5781aeb92a8e6ddbf826ea1b2e
gauri <gaurigera@gmail.com>
2024-08-09 23:43:35 +0530

    New change to rectify deployment

--- src/components/model/Message.jsx
+++ src/components/model/Message.jsx
@@ -1,7 +1,7 @@
 export function Message() {
   return (
     <div className="w-fit h-fit max-w-60 text-xs text-pretty -ml-5">
-      <p className="m-auto">Hover on the logo and have fun!</p>
+      <p className="m-auto">Hover on the logo and have fun! </p>
     </div>
   );
 }
